3ad453f5952404ce397644c88670968dc3ce55f5
Jonasz <s27202@pjwstk.edu.pl>
2024-03-21 19:45:23 +0100

    dodano kontenerowiec

---
+++ Kontenerowiec/ConsoleApp1/Kontener.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleApp1;
+
+public class Kontener
+{
+    public int masa { get; set; }
+    public int wysokosc{ get; set; }
+    public int waga_własna{ get; set; }
+    public int Glebokosc{ get; set; }
+    public String numer_seryjny{ get; set; }
+    public int max_masa{ get; }
+    private int nnumer=0;
+    
+    public Kontener(int masaladunku, int Wysokosc, int wagawlasna, int glebokosc)
+    {
+        masa=masaladunku ;
+        wysokosc = Wysokosc;
+        waga_własna = wagawlasna;
+        Glebokosc = glebokosc;
+        numer_seryjny = GenerujNumerSeryjny();
+    }
+
+    private string GenerujNumerSeryjny()
+    {
+        string rodzajKontenera = this.GetType().Name.Substring(0, 3).ToUpper();
+        int numer = nnumer++;
+        return $"{rodzajKontenera}-C-{numer}";
+    }
+
+    public void oproznijladunek()
+    {
+        masa = 0;
+    }
+
+    public virtual void zaladujladunek(int Masa)
+    {
+        throw new NotImplementedException();
+    }
+    
+
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
